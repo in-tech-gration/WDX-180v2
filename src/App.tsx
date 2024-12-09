4428e90fb2996b0bbad1ed3ae5d1cc8721fb1f9c
Asteris Giannoudis <asteris.gian@gmail.com>
2024-12-09 18:37:45 +0200

    Installed TailwindCSS and changed main page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="bg-white dark:bg-slate-800 text-slate-900 dark:text-white flex flex-col justify-center min-h-screen text-center gap-10">
+      <h1 className="text-3xl">WDX-180</h1>
+      <p className="text-xl text-slate-500 dark:text-slate-400">
+        Web Development X
+      </p>
+      <p className="text-xl text-slate-500 dark:text-slate-400">
+        This is Web Development X 180°. An introductory course to the web and
+        related technologies such as HTML, CSS and JavaScript, split into 180
+        days.
+      </p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="p-2 bg-indigo-500 rounded-md shadow-lg"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
