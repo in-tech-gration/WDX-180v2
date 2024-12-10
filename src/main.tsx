c3a7faaab551ecbb74e3be0e005670bdf052d1b7
Kostas Minaidis <minaidisk@gmail.com>
2024-12-10 02:54:40 +0200

    Install Tailwind UI dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// import Templates from "./Templates.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* Just here for testing the Tailwind UI Templates: */}
+    {/* <Templates /> */}
     <App />
   </StrictMode>
 )
