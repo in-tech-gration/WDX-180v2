37e6e1b859fc1ed061de594e3ad10902d9da56db
Asteris Giannoudis <asteris.gian@gmail.com>
2024-12-16 12:13:09 +0200

    Replaced 'react-router-dom' with 'react-router' package.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router'
 import Home from './pages/Home'
 import About from './pages/About'
 import PageNotFound from './pages/PageNotFound'
